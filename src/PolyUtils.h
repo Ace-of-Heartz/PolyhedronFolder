5eb1631a91978c8a9f846a41eacded911669d87c
Ace-of-Heartz <synthetace@gmail.com>
2024-12-19 07:46:37 +0100

    Utils added

--- src/PolyUtils.h
+++ src/PolyUtils.h
@@ -4,19 +4,34 @@
 
 #ifndef POLYUTILS_H
 #define POLYUTILS_H
+#include <unordered_map>
+
 #include "GLUtils.hpp"
 
 
 class PolyUtils {
 public:
     PolyUtils();
-    static MeshObject<Vertex> ConstrPolyFace(int n) {
+    static MeshObject<Vertex> ConstrPolyFace(int n);
 
+    float GetDefaultAngle(const int& n,const int& m) const {
+        return defaultAngles.at(std::make_pair(n,m));
+    }
+
+    float GetDefaultAngle(const std::pair<int,int>& n) const {
+        return defaultAngles.at(n);
+    }
+
+    void SetDefaultAngle(const int& n, const int& m, float angle) {
+        defaultAngles[std::make_pair(n,m)] = angle;
+    }
+
+    void SetDefaultAngle(const std::pair<int,int>& n, float a) {
+        defaultAngles.at(n) = a;
     }
 
 private:
-
-
+    std::unordered_map<std::pair<int,int>, float> defaultAngles;
 };
 
 
