32f58e50feb77c415122d7ff87399b1f5d29d939
Ace-of-Heartz <synthetace@gmail.com>
2024-12-31 17:54:50 +0100

    Added a compact ImGui widget rendering class
    Light widget added

---
+++ src/ImGuiWidgets.h
@@ -0,0 +1,19 @@
+//
+// Created by ace on 2024.12.31..
+//
+
+#ifndef IMGUILIGHT_H
+#define IMGUILIGHT_H
+
+#include "imgui.h"
+#include "Light.h"
+
+class ImGuiWidgets {
+public:
+    static void RenderLightGUI(Light&);
+    //static void RenderMaterialGUI();
+};
+
+
+
+#endif //IMGUILIGHT_H
