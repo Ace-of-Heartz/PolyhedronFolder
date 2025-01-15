7acb5ae7aabb3781db00ccd8b99b30a5410c56a2
Ace-of-Heartz <synthetace@gmail.com>
2025-01-15 11:41:46 +0100

    Added function for querying list of pivot values

--- src/PolyUtils.h
+++ src/PolyUtils.h
@@ -5,6 +5,7 @@
 #ifndef POLYUTILS_H
 #define POLYUTILS_H
 #include <map>
+#include <optional>
 #include <unordered_map>
 
 #include "GLUtils.hpp"
@@ -50,6 +51,7 @@ namespace PolyhedronFolder {
             }
 
             defaultAngles.insert(std::make_pair(keyVal,angle));
+            nValues.insert(n); nValues.insert(m);
         }
 
         static void SetDefaultAngle(const std::pair<uint,uint>& n, float a) {
@@ -60,15 +62,40 @@ namespace PolyhedronFolder {
                 defaultAngles.erase(keyVal);
 
             defaultAngles.insert(std::make_pair(keyVal,a));
+            nValues.insert(n.first); nValues.insert(n.second);
         }
 
         static glm::mat4 CalcTransformMtx(uint toEdge,float n,float parentN);
 
         static float CalcDefaultAngleBetween(float n, float m, float o);
 
+        static std::vector<std::pair<std::pair<uint,uint>,std::optional<float>>> GetDefaultAngleList()
+        {
+            std::vector<std::pair<std::pair<uint,uint>,std::optional<float>>> res;
+            res.reserve(nValues.size() * nValues.size());
+            for(auto &n : nValues)
+            {
+                for (auto &m : nValues)
+                {
+                    try
+                    {
+                        res.push_back(std::make_pair(std::make_pair(n,m),GetDefaultAngle(n,m)));
+                    } catch (...)
+                    {
+                        res.push_back(std::make_pair(std::make_pair(n,m),std::optional<float>()));
+                    }
+                }
+            }
+            return res;
+        }
 
+        static std::set<uint> GetNValues()
+        {
+            return nValues;
+        }
     private:
         inline static std::unordered_map<size_t, float> defaultAngles;
+        inline static std::set<uint> nValues;
     };
 
 
