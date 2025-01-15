2835c0b66089c8d63643b91c98f5db0db91cbec0
Ace-of-Heartz <synthetace@gmail.com>
2025-01-15 18:44:45 +0100

    Refactor in ImGuiWidgets
    + Added way of listing polygons, and being able to generally interact with them better

--- src/ImGuiWidgets.cpp
+++ src/ImGuiWidgets.cpp
@@ -41,18 +41,140 @@ void ImGuiWidgets::RenderImage(uint image_id, ImVec2 size, ImVec2 uv1, ImVec2 uv
     ImGui::Image(image_id,size,uv1,uv2,tint_col,border_col);
 }
 
-void ImGuiWidgets::PolyFaceButton(int& id,PolyhedronFolder::PolyhedronFace*& face, PolyhedronFolder::Polyhedron& poly)
+void ImGuiWidgets::PolygonTableItem(const int& idx,PolyhedronFolder::PolyhedronFace*& face, PolyhedronFolder::Polyhedron& poly)
 {
 
-    ImGui::Bullet(); ImGui::SameLine();
-    auto name = std::format("{}. {}-NGON",id,face->GetEdgeCount());
+    // ImGui::Bullet(); ImGui::SameLine();
+    auto name = std::format("{}-NGON",face->GetEdgeCount());
 
-    if (ImGui::Button(name.data()))
+    ImGui::Text("%d",idx);
+    ImGui::TableNextColumn();
+
+    ImGui::Text("%s",name.data());
+    ImGui::TableNextColumn();
+
+    ImGui::PushID(idx);
+    int selected;
+
+    if (face->IsActive())
+    {
+        selected = idx;
+    }
+    else
+    {
+        selected = -1;
+    }
+
+    if (ImGui::RadioButton(std::to_string(idx).data(),selected == idx))
     {
         poly.SetActiveFace(face);
     }
+
+
+    ImGui::TableNextColumn();
+
+
+    ImGui::PushID(idx);
+    if(ImGui::ColorButton("",
+        face->GetFreezeState() == PolyhedronFace::UNFREEZE ? ImVec4(1.0,0.6,0.2,1.0)
+        : face->GetFreezeState() == PolyhedronFace::FREEZE_NODE ? ImVec4(0.3,0.44,1.0,1.0) : (ImVec4(0.0,0.0,1.0,1.0)) ))
+    {
+        face->ToggleFreeze();
+    }
+
+    ImGui::TableNextColumn();
+
+    ImGui::BeginDisabled(face->GetFreezeState() == PolyhedronFace::UNFREEZE);
+    auto anim = face->GetAnimationState();
+
+    ImGui::PushID(idx);
+    if(ImGui::SliderFloat("",&anim,0.0f,1.0f))
+    {
+        face->SetAnimationState(anim);
+    }
+    ImGui::PopID();
+    ImGui::PopID();
+    ImGui::PopID();
+
+    ImGui::EndDisabled();
 }
 
+void ImGuiWidgets::RenderPolygonTable(Polyhedron& polyhedron)
+{
+    int idx = 1;
+
+    auto list = polyhedron.GetFaceList();
+
+    auto headers = {"ID","Type","Selected","Freeze","Animation State"};
+    ImGui::BeginTable("Polygons",headers.size());
+    for (auto &h : headers)
+    {
+        ImGui::TableSetupColumn(h);
+    }
+    ImGui::TableHeadersRow();
+
+    for (auto &f : list)
+    {
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+
+        PolygonTableItem(idx,f,polyhedron);
+        ++idx;
+    }
+    ImGui::EndTable();
+}
+
+
+
+void ImGuiWidgets::RenderPivotTable()
+{
+
+    auto nValues = PolyUtils::GetNValues();
+    if(ImGui::BeginTable("Pivot values",nValues.size() + 1,ImGuiTableFlags_Borders))
+    {
+
+        ImGui::TableSetupColumn("N Values");
+        for (auto n : nValues)
+        {
+            ImGui::TableSetupColumn(std::to_string(n).data());
+        }
+
+        ImGui::TableSetupScrollFreeze(0, 1);
+        ImGui::TableHeadersRow();
+
+        // for (auto m : nValues)
+        // {
+        //     ImGui::TableNextColumn();
+        //     ImGui::Text("%i", m);
+        // }
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+
+        for (auto n : nValues)
+        {
+            ImGui::Text("%d",n);
+            for (auto m : nValues)
+            {
+                ImGui::TableNextColumn();
+                try
+                {
+                    auto res = PolyUtils::GetDefaultAngle(n,m);
+                    ImGui::Text("%f",res);
+                } catch (...)
+                {
+                    ImGui::Text("N/A"); //No Angle
+                }
+            }
+            ImGui::TableNextRow();
+            ImGui::TableNextColumn();
+
+
+        }
+        ImGui::EndTable();
+    }
+
+
+}
 
 void ImGuiWidgets::ColorPickerWithButton(const std::string& label,const std::string& popup_id, glm::vec3& color_comp)
 {
